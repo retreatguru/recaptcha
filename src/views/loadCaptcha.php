49ae19fadbccf18eb7e5e0305964953e375b956a
Deryk Wenaus <deryk@retreat.guru>
2017-04-13 13:06:25 -0500

    remove debugger

--- src/views/loadCaptcha.php
+++ src/views/loadCaptcha.php
@@ -1,8 +1,6 @@
 <!-- copied from https://github.com/dancodes/recaptcha/commit/a530617a1f476c734063a1e2a69f1db03c9dac2e -->
 <script type="text/javascript">
     if(typeof grecaptcha === "undefined") {
-        debugger;
-
         var recaptcha_script = document.createElement('script');
         recaptcha_script.src = 'https://www.google.com/recaptcha/api.js?onload=grecaptchaCallback&render=explicit<?php echo (isset($lang) ? '&hl='.$lang : '') ?>';
         document.getElementsByTagName('head')[0].appendChild(recaptcha_script);
